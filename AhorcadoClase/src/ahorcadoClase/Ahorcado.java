c0f94f11a3e273e1a9a212832723e551fc5e8b76
michaelBedDev <micama2000@gmail.com>
2024-01-06 20:32:47 +0100

    Versión 0.1

--- AhorcadoClase/src/ahorcadoClase/Ahorcado.java
+++ AhorcadoClase/src/ahorcadoClase/Ahorcado.java
@@ -5,78 +5,188 @@ import java.util.Scanner;
 
 public class Ahorcado {
 
-	private static Random random = new Random();
-	private static String[] listaPalabras = {"aaaaaaa","bbbbbb","ccasdfagh","ddasdahohgsadñiu","eereiugtwehr"};
-	public static String palabra = listaPalabras [random.nextInt(0, listaPalabras.length - 1)];
-	public static char letrasJugadas [] = new char [27];
-	public static String palabraAsteriscos =  new String (new char[palabra.length()]).replace("/0","*");
-	private static int contador = 0;
-	private static Scanner sc;
+	private Random random = new Random();
+	private Scanner sc;
 	
 	
-	public static void main(String[] args) {
+	private String[] listaPalabras = {"casa","cazo","mandarina","baloncesto"};
+	private String palabra;
+	private String palabraAsteriscos;
+	private char letrasJugadas [] = new char [27]; //Maximo
+	private int contadorAhorcado = 0;
+
+	
+	
+	//METODO
+	public void jugar() {
 		
 		sc = new Scanner(System.in);
 		
-		System.out.println("JUEGO AHORCADO");
+		
+		
+		//Creamos nueva palabra, borramos las letras jugadas, contador de errores = 0
+		this.palabra = listaPalabras [random.nextInt(0, listaPalabras.length - 1)];
+		this.palabraAsteriscos = new String(new char[palabra.length()]).replace('\0', '_');
+		borrarLetrasJugadas();
+		this.contadorAhorcado = 0;
+		
+		
+		
+		//Bucle del juego
+		while (contadorAhorcado < 10 && this.palabraAsteriscos.contains("_")){
+			
+			System.out.println("La palabra es: ");
+			System.out.println(this.palabraAsteriscos);
+	
+			
+			if (this.letrasJugadas[0] != '\0') {
+				System.out.print("Letras jugadas: ");
+			
+				for (char c : this.letrasJugadas) {
+					if (c != '\0')
+						System.out.print(c);
+				}
+				System.out.println();
+			}
+			System.out.println();
+			
+			
+			
+			//Pedir input al usuario
+			String inputUsuario;
+			char letraEscogida;
+			boolean letraRepetida = false;
+			
+			do {
+				do {
+					System.out.println("Jugar la letra (o la palabra si la has adivinado)");
+					inputUsuario = sc.next();
+					letraEscogida = inputUsuario.charAt(0);
+					
+				} while (inputUsuario.length() == 0) ; //Se repite el input del usuario siempre que el input sea vacío
+				
+				
+				if (inputUsuario.length() == 1)
+					letraRepetida = comprobarLetraRepetida(letraEscogida);
+	
+			} while (letraRepetida == true);
+			
+			
+			//añadimos letra a letras jugadas
+			for (int i = 0; i < this.letrasJugadas.length; i++) {
+				if (this.letrasJugadas[i] == '\0') {
+					this.letrasJugadas[i] = letraEscogida;
+					break;
+				}
+				else {
+					
+				}
+			}
+			
+			
+			
+			//jugar una palabra
+			
+			if (inputUsuario.length() > 1) {
+				
+				if (inputUsuario.equals(palabra)) {
+					System.out.println("Enhorabuena! Has ganado. La palabra era " + this.palabra);
+					return;
+				}
+				else {
+					contadorAhorcado++;
+					imprimirDibujo(contadorAhorcado);
+				}  
+			}
+			//jugar una letra
+			else {
+				jugarLetra(letraEscogida);	
+			}
+		
+		
+			if (this.contadorAhorcado == 10)
+				System.err.println("Perdiste. Inténtalo de nuevo");
+		
+			System.out.println();
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	//comprobar letra repetida
+	private boolean comprobarLetraRepetida (char letraEscogida) {
+		
+		for(char c : this.letrasJugadas) {
+			
+			if (c == letraEscogida) {
+				System.err.println("Letra Repetida");
+				System.out.println("Por favor, introduce una nueva letra");
+				System.out.println();
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	//borrar letras jugadas
+	private void borrarLetrasJugadas() {
+		for (char c : this.letrasJugadas) {
+			if (c != '\0')
+				c = '\0';
+		}
+	}
+	
+	
+	
+
+	//Jugar la letra
+	private void jugarLetra(char letraEscogida) {
+
+		String nuevoAsteriscos = "";
+		boolean letraEstáEnPalabra = false;
+		
+		//bucle para crear los asteriscos nuevos
+		for(int i = 0; i< this.palabra.length(); i++){
+			if (letraEscogida == this.palabra.charAt(i)) {
+				nuevoAsteriscos += letraEscogida;
+				letraEstáEnPalabra = true;
+			}
+			else if (this.palabraAsteriscos.charAt(i) != '_') {
+                nuevoAsteriscos += this.palabra.charAt(i);
+            }
+			else 
+				nuevoAsteriscos += "_";
+		}
+		
+		this.palabraAsteriscos = nuevoAsteriscos;
+		
+		
+		//si la palabra no está imprimimos dibujo
+		if (letraEstáEnPalabra == false) {
+			contadorAhorcado++;
+			imprimirDibujo(contadorAhorcado);
+		}
+
+		if (this.palabraAsteriscos.equals(this.palabra))
+			System.out.println("Enhorabuena! Has ganado. La palabra era " + this.palabra);
+	}
+	
+	
+	
+
+
+	//imprimir dibujo
+	private void imprimirDibujo(int contador) {
+		
 		System.out.println();
 		
-		jugarLetra();
-	
-		
-	}
-	
-	
-	private static void jugarLetra() {
-		
-		boolean letraRepetida; char letraEscogida;
-		
-		do {
-			do {
-				System.out.println("Jugar la letra");
-				letraEscogida = sc.next().charAt(0);
-				letraRepetida = false;
-				
-				for(char c : letrasJugadas) {
-					
-					if (c == '\0') {
-						c = letraEscogida;
-						break;
-					}
-					
-					if (c == letraEscogida) {
-						System.err.println("Letra Repetida");
-						System.out.println("Por favor, introduce una nueva letra");
-						letraRepetida = true;
-						break;
-					}	
-				}
-			} while (letraRepetida);
-			
-			//Arreglar
-			boolean letraEstá = false;
-			for(int i = 0; i<palabra.length(); i++){
-				if (letraEscogida == palabra.charAt(i)) {
-					palabraAsteriscos.replace(palabraAsteriscos.charAt(i), letraEscogida);
-					letraEstá = true;
-				}
-			}
-			
-			if (letraEstá == false) {
-				contador++;
-				imprimirDibujo(contador);
-			}
-
-			
-			System.out.println(palabraAsteriscos);
-			
-	    } while (contador < 10 || !palabraAsteriscos.equals(palabra));
-		
-		
-		System.out.println(palabraAsteriscos.equals(palabra) ? "Enhorabuena! Has Ganado." : "Oh no, perdiste. Inténtalo de nuevo");			
-	}
-	
-	private static void imprimirDibujo(int contador) {
 		switch (contador) {
 		case 1:
 			System.out.println("+------");
@@ -109,7 +219,11 @@ public class Ahorcado {
 			System.out.println("+----+\n|    |\n|    ()\n|   /[]\\\n|    /\\\n+------");
 			break;
 		}
+		
 	}
-	
-	
+
 }
+	
+
+	
+
---
+++ AhorcadoClase/src/ahorcadoClase/App.java
@@ -0,0 +1,17 @@
+package ahorcadoClase;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		
+		Ahorcado hangman = new Ahorcado();
+		
+		System.out.println("JUEGO AHORCADO");
+		System.out.println();
+		
+		hangman.jugar();
+		
+	}
+
+}
