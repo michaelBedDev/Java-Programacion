7c4cd68077756ae9b3ebfc88ac192fda871eab79
Miguel <Miguel@DESKTOP-O9K6SD3>
2024-02-09 17:59:41 +0100

    Falta getPrevious()

--- UD7-ColeccionesDeObjetos/src/ejercicio6listas/Avion.java
+++ UD7-ColeccionesDeObjetos/src/ejercicio6listas/Avion.java
@@ -1,77 +1,103 @@
 package ejercicio6listas;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
-public class Avion implements Ejercicio6<Pasajero>{
+public class Avion implements Ejercicio6<Pasajero> {
 	private String origen;
 	private String destino;
 	private int plaza;
 	private boolean business;
 	private LinkedList<Pasajero> listaPasajeros;
-	//iterator
-	
-	
-	
-	
+	private Iterator<Pasajero> it = listaPasajeros.iterator();
+
 	@Override
 	public void insert(Pasajero x) {
 		listaPasajeros.add(x);
-		
+
 	}
+
 	@Override
 	public void remove(Pasajero x) {
-		listaPasajeros.remove(0);
+
+		boolean flagEliminado = false;
+		while (it.hasNext()) {
+			Pasajero p = (Pasajero) it.next();
+
+			if (p.equals(x)) {
+				it.remove();
+				flagEliminado = true;
+			}
+
+		}
+
+		// hacelo con try catch
+		if (!flagEliminado) {
+			System.out.println("No se ha encontrado al pasajero para eliminarlo");
+		}
 	}
+
 	@Override
 	public void removeCurrent() {
-		// TODO Auto-generated method stub
-		
+		it.remove();
 	}
+
 	@Override
 	public boolean find(Pasajero x) {
-		// TODO Auto-generated method stub
+		while (it.hasNext()) {
+			Pasajero p = (Pasajero) it.next();
+
+			if (it.equals(p)) {
+				return true;
+			}
+		}
 		return false;
 	}
+
 	@Override
 	public void goFirst() {
-		// TODO Auto-generated method stub
-		
+		// ?
+		it = listaPasajeros.listIterator(0);
 	}
+
 	@Override
 	public void advance() {
-		// TODO Auto-generated method stub
-		
+		it.next();
 	}
+
 	@Override
-	public boolean isOnList() {
-		// TODO Auto-generated method stub
+	public boolean isOnList() { //no entiendo muy bien lo del metodo
+		while (it.hasNext()) {
+			Pasajero p = (Pasajero) it.next();
+
+			if (p == null) {
+				return true;
+			}
+
+		}
 		return false;
 	}
+
 	@Override
 	public Object getCurrent() {
-		// TODO Auto-generated method stub
-		return null;
+		//?
+		
+		Object o = it.next();
+		
+		return o;
 	}
+
 	@Override
 	public Object getPrevious() {
-		// TODO Auto-generated method stub
+		// return objeto al que apunta el iterator -1
 		return null;
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	protected Avion() {
 		super();
-		//TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stub
 	}
+
 	protected Avion(String origen, String destino, int plaza, boolean business) {
 		super();
 		this.origen = origen;
@@ -79,28 +105,35 @@ public class Avion implements Ejercicio6<Pasajero>{
 		this.plaza = plaza;
 		this.business = business;
 	}
-	
+
 	public String getOrigen() {
 		return origen;
 	}
+
 	public void setOrigen(String origen) {
 		this.origen = origen;
 	}
+
 	public String getDestino() {
 		return destino;
 	}
+
 	public void setDestino(String destino) {
 		this.destino = destino;
 	}
+
 	public int getPlaza() {
 		return plaza;
 	}
+
 	public void setPlaza(int plaza) {
 		this.plaza = plaza;
 	}
+
 	public boolean isBusiness() {
 		return business;
 	}
+
 	public void setBusiness(boolean business) {
 		this.business = business;
 	}
