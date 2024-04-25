e6562ea125d2e304525529de5b0ab54a2c35877f
michaelBedDev <micama2000@gmail.com>
2024-04-25 22:42:47 +0200

    .env added

--- ZZMariaDB/src/db_tarea_ud9/AccessToDB.java
+++ ZZMariaDB/src/db_tarea_ud9/AccessToDB.java
@@ -15,16 +15,16 @@ public class AccessToDB {
 	private AccessToDB() {
 
 			dataSource = new BasicDataSource();
-			dataSource.setUrl("jdbc:mariadb://dbalumnos.sanclemente.local:3314/MCMpodcast_bd?tinyInt1isBit=false");
-			dataSource.setUsername("alumno");
-			dataSource.setPassword("abc123..");
-			dataSource.setInitialSize(5);
-			dataSource.setMaxTotal(10);
+//			dataSource.setUrl("jdbc:mariadb://dbalumnos.sanclemente.local:3314/MCMpodcast_bd?tinyInt1isBit=false");
+//			dataSource.setUsername("alumno");
+//			dataSource.setPassword("abc123..");
+//			dataSource.setInitialSize(5);
+//			dataSource.setMaxTotal(10);
 
-//			Dotenv dotenv = Dotenv.load();
-//			dataSource.setUrl(dotenv.get("URL"));
-//			dataSource.setUsername(dotenv.get("user"));
-//			dataSource.setPassword(dotenv.get("pass"));
+			Dotenv dotenv = Dotenv.load();
+			dataSource.setUrl(dotenv.get("URL"));
+			dataSource.setUsername(dotenv.get("user"));
+			dataSource.setPassword(dotenv.get("pass"));
 	}
 
 	public BasicDataSource getDataSource() {
